b85ad2282e53b6cfd1707e43bd2e78ad0f3cb97f
Eddie Fann <efann@beowurks.com>
2026-02-24 10:24:27 -0600

    Updated batch file to check for python interpreter.

--- generateMaster.bat
+++ generateMaster.bat
@@ -2,10 +2,24 @@
 
 @REM Get path of script
 set SCRIPT_PATH=%~dp0
+set PYTHON=%PROGRAMFILES%\LibreOffice\program\python.exe
+
+echo Path of python interpreter: %PYTHON%
+REM echo. is just a blank line.
+echo.
+
+if not exist "%PYTHON%" (
+    echo %PYTHON% cannot be found.
+    echo Ensure that LibreOffice has been installed.
+    echo.
+    pause
+    exit
+)
 
 echo cd %SCRIPT_PATH%
 cd "%SCRIPT_PATH%"
 
-"%PROGRAMFILES%\LibreOffice\program\python.exe" main.py
+REM You need the quotes around PYTHON as it contains spaces.
+"%PYTHON%" main.py
 
 pause
